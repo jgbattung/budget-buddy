bee0a7696633613004c3778baaba6191014d3bec
Jireh Battung <jirehgbattung@gmail.com>
2023-05-29 15:45:28 +0800

    update firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import firebase from 'firebase/app';
-import { firebaseConfig } from './firebaseConfig';
+import { firebaseConfig } from './config/firebaseConfig';
 import 'firebase/auth';
+import { initializeApp } from "firebase/app";
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
